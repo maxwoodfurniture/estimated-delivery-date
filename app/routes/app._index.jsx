98c8be529b830b97c6262feddc97943bf10b6eaf
ian-dudderar <dudderari@gmail.com>
2026-02-10 08:12:49 -0500

    Prisma Structure

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -11,68 +11,69 @@ export const loader = async ({ request }) => {
 };
 
 export const action = async ({ request }) => {
-  const { admin } = await authenticate.admin(request);
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
-  const response = await admin.graphql(
-    `#graphql
-      mutation populateProduct($product: ProductCreateInput!) {
-        productCreate(product: $product) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }`,
-    {
-      variables: {
-        product: {
-          title: `${color} Snowboard`,
-        },
-      },
-    },
-  );
-  const responseJson = await response.json();
-  const product = responseJson.data.productCreate.product;
-  const variantId = product.variants.edges[0].node.id;
-  const variantResponse = await admin.graphql(
-    `#graphql
-    mutation shopifyReactRouterTemplateUpdateVariant($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
-      productVariantsBulkUpdate(productId: $productId, variants: $variants) {
-        productVariants {
-          id
-          price
-          barcode
-          createdAt
-        }
-      }
-    }`,
-    {
-      variables: {
-        productId: product.id,
-        variants: [{ id: variantId, price: "100.00" }],
-      },
-    },
-  );
-  const variantResponseJson = await variantResponse.json();
+  // const { admin } = await authenticate.admin(request);
+  // const color = ["Red", "Orange", "Yellow", "Green"][
+  //   Math.floor(Math.random() * 4)
+  // ];
+  // const response = await admin.graphql(
+  //   `#graphql
+  //     mutation populateProduct($product: ProductCreateInput!) {
+  //       productCreate(product: $product) {
+  //         product {
+  //           id
+  //           title
+  //           handle
+  //           status
+  //           variants(first: 10) {
+  //             edges {
+  //               node {
+  //                 id
+  //                 price
+  //                 barcode
+  //                 createdAt
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }`,
+  //   {
+  //     variables: {
+  //       product: {
+  //         title: `${color} Snowboard`,
+  //       },
+  //     },
+  //   },
+  // );
+  // const responseJson = await response.json();
+  // const product = responseJson.data.productCreate.product;
+  // const variantId = product.variants.edges[0].node.id;
+  // const variantResponse = await admin.graphql(
+  //   `#graphql
+  //   mutation shopifyReactRouterTemplateUpdateVariant($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
+  //     productVariantsBulkUpdate(productId: $productId, variants: $variants) {
+  //       productVariants {
+  //         id
+  //         price
+  //         barcode
+  //         createdAt
+  //       }
+  //     }
+  //   }`,
+  //   {
+  //     variables: {
+  //       productId: product.id,
+  //       variants: [{ id: variantId, price: "100.00" }],
+  //     },
+  //   },
+  // );
+  // const variantResponseJson = await variantResponse.json();
 
-  return {
-    product: responseJson.data.productCreate.product,
-    variant: variantResponseJson.data.productVariantsBulkUpdate.productVariants,
-  };
+  // return {
+  //   product: responseJson.data.productCreate.product,
+  //   variant: variantResponseJson.data.productVariantsBulkUpdate.productVariants,
+  // };
+  return null;
 };
 
 export default function Index() {
